a99fe077b936f3ad8fff51c1936d9ea97aae8281
Ana Cornachi <85295984+anacornachi@users.noreply.github.com>
2022-11-04 22:36:33 -0300

    finishes web interface with revalidate cache

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,19 +5,21 @@ export default function Document() {
     <Html>
       <Head>
         <div>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin=""
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,400;1,700&display=swap"
-            rel="stylesheet"
-          />
+          <div>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+              rel="preconnect"
+              href="https://fonts.gstatic.com"
+              crossOrigin=""
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,700&display=swap"
+              rel="stylesheet"
+            />
+          </div>
         </div>
       </Head>
-      <body className="bg-gray-900">
+      <body className="bg-gray-900 bg-app bg-no-repeat bg-cover">
         <Main />
         <NextScript />
       </body>
